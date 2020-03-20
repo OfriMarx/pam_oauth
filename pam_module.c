cb4cea0817cd27868a2c09e7ce0135b50743a81b
Ofri Marx <ofrikm@gmail.com>
2020-03-20 16:47:41 +0200

    module now authenticates according to name and pass in config file

--- pam_module.c
+++ pam_module.c
@@ -1,5 +1,4 @@
 #define PAM_SM_AUTH
-//#define PAM_SM_ACCOUNT
 
 #include <security/pam_modules.h> 
 #include <string.h>
@@ -15,26 +14,87 @@
 #define LOG_FUNC(msg, ...) NULL
 #endif
 
+typedef struct pam_conv pam_conv;
+typedef struct pam_response pam_response;
+typedef struct pam_message pam_message;
+
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t* pamh, int flag, int argc, const char** argv)
 {
 	const char* username = NULL;
+   	char* password = NULL;
 	FILE* configFile = NULL;
 	char* line = NULL, *name = NULL, *pass = NULL;
 	char* retval = NULL; 
 	int authenticated = PAM_AUTH_ERR;
+	pam_conv* passConv;
+	pam_message* msg = NULL;
+	pam_response* resp = NULL;
 
 	openlog("pamlog2", LOG_CONS, LOG_USER);
 	LOG_FUNC("cool beans");
 
-	if(pam_get_user(pamh, &username, "pass for totaly secure system: ") != PAM_SUCCESS)
+	if(pam_get_user(pamh, &username, NULL) != PAM_SUCCESS)
 	{
 		LOG_FUNC("couldn't get username");
 		closelog();
 		return PAM_AUTH_ERR;
 	}
 
+	if(pam_get_item(pamh, PAM_CONV, (const void**)&passConv) != PAM_SUCCESS)
+	{
+		LOG_FUNC("couldn't get password conv");
+		closelog();
+		return PAM_AUTH_ERR;
+	}
+
+	msg = (pam_message*)malloc(sizeof(pam_message));
+	if(msg == NULL)
+	{
+		LOG_FUNC("bad allocation :(");
+		closelog();
+		return PAM_AUTH_ERR;
+	}
+
+	msg->msg_style = PAM_PROMPT_ECHO_OFF;
+	msg->msg = NULL;
+	if(passConv->conv(1, (const pam_message**)&msg, &resp, NULL) != PAM_SUCCESS)
+	{
+		free(msg);
+		LOG_FUNC("couldn't password");
+		closelog();
+		return PAM_AUTH_ERR;
+	}
+	
+	if(resp->resp_retcode != PAM_SUCCESS)
+	{
+		free(msg);
+		free(resp->resp);
+		free(resp);
+		LOG_FUNC("convo func failed");
+		closelog();
+		return PAM_AUTH_ERR;
+	}
+
+	if((password = strdup(resp->resp)) == NULL)
+	{
+		free(msg);
+		free(resp->resp);
+		free(resp);
+		LOG_FUNC("bad alloc :(");
+		closelog();
+		return PAM_AUTH_ERR;
+	}
+
+	free(msg);
+	free(resp->resp);
+	free(resp);
+
+	LOG_FUNC("username: %s", username);
+	LOG_FUNC("password: %s", password);
+
 	if((configFile = fopen("/etc/pasten.conf", "r")) == NULL)
 	{
+		free(password);
 		LOG_FUNC("couldn't open config file");
 		closelog();
 		return PAM_AUTH_ERR;
@@ -44,20 +104,20 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t* pamh, int flag, int argc, const
 
 	if(line == NULL)
 	{
+		free(password);
 		fclose(configFile);
 		LOG_FUNC("bad allocation :(");
 		closelog();
 		return PAM_AUTH_ERR;
 	}
 
-	LOG_FUNC("successfull setup");
+	LOG_FUNC("successful setup");
 
 	retval = fgets(line, 200, configFile);
 	while(retval == line)
 	{
 		if(line[strlen(line) - 1] == '\n')
 			line[strlen(line) - 1] = 0;
-		LOG_FUNC("line: %s", line);
 		
 		pass = strchr(line, '=');
 		if(pass != NULL)
@@ -67,7 +127,7 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t* pamh, int flag, int argc, const
 			name = line;
 			LOG_FUNC("name: %s, pass: %s", name, pass);
 
-			if(strcmp(name, username) == 0)
+			if(strcmp(name, username) == 0 && strcmp(pass, password) == 0)
 			{
 				authenticated = PAM_SUCCESS;
 				break;
@@ -76,6 +136,7 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t* pamh, int flag, int argc, const
 		retval = fgets(line, 200, configFile);
 	}
 
+	free(password);
 	free(line);
 	fclose(configFile);
 
@@ -89,8 +150,3 @@ PAM_EXTERN int pam_sm_setcred(pam_handle_t* pamh, int flag, int argc, const char
 {
 	return PAM_SUCCESS;
 }
-
-//PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t* pamh, int flag, int argc, const char** argv)
-//{
-//	return PAM_SUCCESS;
-//}
