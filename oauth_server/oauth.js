1c3e174136563d98fa244820076b426695f3a650
Ofri Marx <ofrikm@gmail.com>
2020-03-27 00:23:51 +0200

    added basic http authentication to server

--- oauth_server/oauth.js
+++ oauth_server/oauth.js
@@ -1,3 +1,20 @@
+const fs = require("fs");
+
+
+var lines = fs.readFileSync("/etc/pasten.conf").toString().split("\n");
+var creds = {};
+for(let i=0; i < lines.length; i++) {
+	if(!lines[i])
+		continue;
+
+	let line = lines[i].split("=");
+	creds[line[0]] = line[1];
+}
+
 exports.generate_token = function() {
 	return "token123";
+}
+
+exports.authenticate = function(client_id, client_secret) {
+	return creds[client_id] === client_secret;
 }
--- oauth_server/server.js
+++ oauth_server/server.js
@@ -2,8 +2,25 @@ const http = require("http");
 const url = require("url");
 const oauth = require("./oauth");
 
+
 http.createServer(function(req, res) {
 	var path = url.parse(req.url).pathname; 
+	var headers = req.headers;
+
+	if(! ('authorization' in headers)) {
+		res.writeHead(401, {'Content-Type': 'text/plain',
+			'WWW-Authenticate': 'Basic realm="Access to OAUTH 2 server"'});
+		res.end("unauthorized");
+		return;
+	}
+	else {
+		let creds = Buffer.from(headers['authorization'].split(" ")[1], "base64").toString().split(":");
+		if(!oauth.authenticate(creds[0], creds[1])) {
+			res.writeHead(403, {'Content-Type': 'text/plain'});
+			res.end("forbidden");
+			return;
+		}
+	}
 
 	if(path === "/token") {
 		res.writeHead(200, {'Content-Type': 'text/plain'});
